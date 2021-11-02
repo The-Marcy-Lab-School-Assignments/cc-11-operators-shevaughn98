ff112a638cb1bd590a9e86f776c1e940132f218c
shevaughn98 <shevaughnlegall@gmail.com>
2021-11-02 14:55:22 +0000

    cc

--- index.js
+++ index.js
@@ -1,27 +1,56 @@
 //1
-function areBothSame(){
+// function areBothSame(num){
+//     if(num == 1){
+//      return "true"   
+//     }else{
+//         return "false"
+//     }
   
-}
-
+// }
+// console.log(areBothSame(1))
+//make if statement i think if true equals 1 then return true or false.
 //2
-function areBothEqual(){
+// function areBothEqual(num1,num2){
+//     if (num1 ===  num2 ){
+//         return "true"
+//     }else{
+//         return "false"
+//     }
+    
   
-}
-
+// }
+// console.log(areBothEqual("2",2))
 //3
-function areAllFourEqual(){
+// function areAllFourEqual(num1,num2,num3,num4){
+//     if (num1 === num2 && num3 === num4){
+//         return "true"
+//     }else{
+//         return "false"
+//     }
   
-}
-
+// }
+// console.log(areAllFourEqual("2","2","2",2))
 
 //4
-function areAllFourSame(){
+// function areAllFourSame(num1,num2,num3,num4){
+//       if (num1 == num2 && num3 == num4){
+//         return "true"
+//     }else{
+//         return "false"
+//     }
   
-}
-
+// }
+// console.log(areAllFourSame(true,true,1,"1"))
 //5
-function areAllFourSameOrEqual(){
+// function areAllFourSameOrEqual(num1,num2,num3,num4){
+//     if(num1 == num2 && num2 == num3 && num3 == num4){
+//         return "same"
+//     }else if(num1 === num2 && num2 === num3 && num3 === num4){
+//         return "equal"
+//     }else{
+//         return "null"
+//     }
   
-}
-
+// }
+// console.log(areAllFourSameOrEqual("2","2","2","2"))
 
